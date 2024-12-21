3b7de482e3b1db81012b6d877d8506baf1c7eb86
Martin Kanev <martin_kk@abv.bg>
2024-12-21 18:52:28 +0200

    Add project files.

---
+++ Tic-Tac-Toe Project/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Tic_Tac_Toe_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStatus = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            btn_restart = new Button();
+            checkedListBox1 = new CheckedListBox();
+            checkedListBox2 = new CheckedListBox();
+            btm_start = new Button();
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(386, 9);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(75, 15);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "Game status:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(149, 123);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 113);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += PlayerMove;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(350, 123);
+            button2.Name = "button2";
+            button2.Size = new Size(143, 113);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += PlayerMove;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(547, 123);
+            button3.Name = "button3";
+            button3.Size = new Size(143, 113);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += PlayerMove;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(149, 286);
+            button4.Name = "button4";
+            button4.Size = new Size(143, 113);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += PlayerMove;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(350, 286);
+            button5.Name = "button5";
+            button5.Size = new Size(143, 113);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += PlayerMove;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(547, 286);
+            button6.Name = "button6";
+            button6.Size = new Size(143, 113);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += PlayerMove;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(149, 448);
+            button7.Name = "button7";
+            button7.Size = new Size(143, 113);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += PlayerMove;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(350, 448);
+            button8.Name = "button8";
+            button8.Size = new Size(143, 113);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += PlayerMove;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(547, 448);
+            button9.Name = "button9";
+            button9.Size = new Size(143, 113);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += PlayerMove;
+            // 
+            // btn_restart
+            // 
+            btn_restart.Location = new Point(375, 616);
+            btn_restart.Name = "btn_restart";
+            btn_restart.Size = new Size(98, 28);
+            btn_restart.TabIndex = 10;
+            btn_restart.Text = "Restart game";
+            btn_restart.UseVisualStyleBackColor = true;
+            btn_restart.Click += btn_restart_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Difficulty - easy", "Difficulty - hard" });
+            checkedListBox1.Location = new Point(732, 50);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(112, 40);
+            checkedListBox1.TabIndex = 11;
+            //checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
+            // 
+            // checkedListBox2
+            // 
+            checkedListBox2.FormattingEnabled = true;
+            checkedListBox2.Items.AddRange(new object[] { "Choose - X", "Choose - O" });
+            checkedListBox2.Location = new Point(24, 50);
+            checkedListBox2.Name = "checkedListBox2";
+            checkedListBox2.Size = new Size(112, 40);
+            checkedListBox2.TabIndex = 12;
+            //checkedListBox2.ItemCheck += checkedListBox2_ItemCheck;
+            // 
+            // btm_start
+            // 
+            btm_start.Location = new Point(353, 58);
+            btm_start.Name = "btm_start";
+            btm_start.Size = new Size(140, 32);
+            btm_start.TabIndex = 13;
+            btm_start.Text = "start game";
+            btm_start.UseVisualStyleBackColor = true;
+            btm_start.Click += btm_start_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(878, 670);
+            Controls.Add(btm_start);
+            Controls.Add(checkedListBox2);
+            Controls.Add(checkedListBox1);
+            Controls.Add(btn_restart);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(lblStatus);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button btn_restart;
+        private CheckedListBox checkedListBox1;
+        private CheckedListBox checkedListBox2;
+        private Button btm_start;
+    }
+}
---
+++ Tic-Tac-Toe Project/Form1.cs
@@ -0,0 +1,478 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tic_Tac_Toe_Project
+{
+    public partial class Form1 : Form
+    {
+
+        private string[,] board = new string[3, 3];
+        private Random rnd = new Random();
+        private Difficulty gameDifficulty;
+        private PlayerType Player;
+        private PlayerType AIPlayer;
+        private Button[,] buttonGrid = new Button[3, 3];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public enum Difficulty
+        {
+            easy,
+            hard
+        }
+
+        public enum PlayerType
+        {
+            X,
+            O
+        }
+
+
+        private void InitializeGame()
+        {
+            
+            bool isXSelected = checkedListBox2.GetItemCheckState(0) == CheckState.Checked;
+            bool isOSelected = checkedListBox2.GetItemCheckState(1) == CheckState.Checked;
+
+            if (isXSelected && !isOSelected)
+            {
+                Player = PlayerType.X;
+                AIPlayer = PlayerType.O;
+            }
+            else if (isOSelected && !isXSelected)
+            {
+                Player = PlayerType.O;
+                AIPlayer = PlayerType.X;
+            }
+            else
+            {
+                MessageBox.Show("Please select exactly one option: X or O.");
+                return;
+            }
+
+           
+            bool isEasySelected = checkedListBox1.GetItemCheckState(0) == CheckState.Checked;
+            bool isHardSelected = checkedListBox1.GetItemCheckState(1) == CheckState.Checked;
+
+            if (isEasySelected && !isHardSelected)
+            {
+                gameDifficulty = Difficulty.easy;
+            }
+            else if (isHardSelected && !isEasySelected)
+            {
+                gameDifficulty = Difficulty.hard;
+            }
+            else
+            {
+                MessageBox.Show("Please select exactly one option: easy or hard.");
+                return;
+            }
+
+            
+            if (Player == PlayerType.O)
+            {
+                AIMove(); 
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private bool CheckGameStatus()
+        {
+            
+            for (int i = 0; i < 3; i++)
+            {
+                
+                if (!string.IsNullOrEmpty(board[i, 0]) && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    MessageBox.Show($"Row {i} victory: {board[i, 0]}");
+                    EndGame($"{board[i, 0]} wins!");
+                    return true;
+                }
+
+                
+                if (!string.IsNullOrEmpty(board[0, i]) && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    MessageBox.Show($"Column {i} victory: {board[0, i]}");
+                    EndGame($"{board[0, i]} wins!");
+                    return true;
+                }
+            }
+
+            
+            if (!string.IsNullOrEmpty(board[0, 0]) && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                MessageBox.Show($"Diagonal 1 victory: {board[0, 0]}");
+                EndGame($"{board[0, 0]} wins!");
+                return true;
+            }
+
+            if (!string.IsNullOrEmpty(board[0, 2]) && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                MessageBox.Show($"Diagonal 2 victory: {board[0, 2]}");
+                EndGame($"{board[0, 2]} wins!");
+                return true;
+            }
+
+            
+            foreach (string cell in board)
+            {
+                if (string.IsNullOrEmpty(cell))
+                {
+                    return false; 
+                }
+            }
+
+            MessageBox.Show("Draw");
+            EndGame("It's a draw!");
+            return true;
+        }
+
+
+
+
+        private void EndGame(string message)
+        {
+            MessageBox.Show(message, "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            RestartGame();
+        }
+
+
+
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    buttonGrid[row, col].Enabled = true;
+                    buttonGrid[row, col].Text = string.Empty;
+                    board[row, col] = null; 
+                }
+            }
+        }
+
+
+
+
+        private void PlayerMove(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            
+            button.Text = Player.ToString();
+            button.Enabled = false;
+
+            // Намираме реда и колоната на бутона в buttonGrid
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (buttonGrid[row, col] == button)
+                    {
+                        
+                        board[row, col] = Player.ToString();
+
+                       
+
+                        
+                        if (!CheckGameStatus())
+                        {
+                            AIMove();
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+        private void AIMove()
+        {
+            if (gameDifficulty == Difficulty.easy)
+            {
+                
+                List<Button> freeButtons = new List<Button>();
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (string.IsNullOrEmpty(board[row, col])) 
+                        {
+                            freeButtons.Add(buttonGrid[row, col]);
+                        }
+                    }
+                }
+
+                if (freeButtons.Count > 0)
+                {
+                    
+                    int index = rnd.Next(freeButtons.Count);
+                    Button aiButton = freeButtons[index];
+
+                    
+                    aiButton.Text = AIPlayer.ToString();
+                    aiButton.Enabled = false;
+
+                    
+                    for (int row = 0; row < 3; row++)
+                    {
+                        for (int col = 0; col < 3; col++)
+                        {
+                            if (buttonGrid[row, col] == aiButton)
+                            {
+                                board[row, col] = AIPlayer.ToString();
+
+                                
+
+                                CheckGameStatus();
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                
+                AIMoveHard();
+            }
+        }
+
+
+
+
+        private void EnableGameButtons()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    buttonGrid[row, col].Enabled = true;
+                }
+            }
+        }
+
+
+
+        private void btm_start_Click(object sender, EventArgs e)
+        {
+
+            EnableGameButtons();
+
+            
+            btm_start.Enabled = false;
+
+            
+            InitializeGame();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonGrid[0, 0] = button1;
+            buttonGrid[0, 1] = button2;
+            buttonGrid[0, 2] = button3;
+            buttonGrid[1, 0] = button4;
+            buttonGrid[1, 1] = button5;
+            buttonGrid[1, 2] = button6;
+            buttonGrid[2, 0] = button7;
+            buttonGrid[2, 1] = button8;
+            buttonGrid[2, 2] = button9;
+
+            
+            foreach (Button button in buttonGrid)
+            {
+                button.Enabled = false;
+            }
+
+            
+            btm_start.Enabled = true;
+        }
+
+
+        
+
+        private int EvaluateBoard()
+        {
+            
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    if (board[i, 0] == AIPlayer.ToString()) return +10;
+                    if (board[i, 0] == Player.ToString()) return -10;
+                }
+            }
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    if (board[0, i] == AIPlayer.ToString()) return +10;
+                    if (board[0, i] == Player.ToString()) return -10;
+                }
+            }
+
+            
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == AIPlayer.ToString()) return +10;
+                if (board[0, 0] == Player.ToString()) return -10;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == AIPlayer.ToString()) return +10;
+                if (board[0, 2] == Player.ToString()) return -10;
+            }
+
+            
+            return 0;
+        }
+
+
+
+
+
+        private bool IsMovesLeft()
+        {
+            foreach (string cell in board)
+            {
+                if (string.IsNullOrEmpty(cell))
+                    return true;
+            }
+            return false;
+        }
+
+
+
+
+
+        private int Minimax(int depth, bool isMaximizingPlayer, int alpha, int beta)
+        {
+            int score = EvaluateBoard();
+
+            
+            if (score == 10) return score - depth; 
+            if (score == -10) return score + depth; 
+
+            
+            if (!IsMovesLeft()) return 0;
+
+            if (isMaximizingPlayer)
+            {
+                int best = int.MinValue;
+
+                
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (string.IsNullOrEmpty(board[row, col]))
+                        {
+                            board[row, col] = AIPlayer.ToString();
+                            best = Math.Max(best, Minimax(depth + 1, false, alpha, beta));
+                            board[row, col] = null;
+
+                            alpha = Math.Max(alpha, best);
+                            if (beta <= alpha)
+                                break;
+                        }
+                    }
+                }
+                return best;
+            }
+            else
+            {
+                int best = int.MaxValue;
+
+               
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (string.IsNullOrEmpty(board[row, col]))
+                        {
+                            board[row, col] = Player.ToString();
+                            best = Math.Min(best, Minimax(depth + 1, true, alpha, beta));
+                            board[row, col] = null;
+
+                            beta = Math.Min(beta, best);
+                            if (beta <= alpha)
+                                break;
+                        }
+                    }
+                }
+                return best;
+            }
+        }
+
+
+
+        private void AIMoveHard()
+        {
+            int bestValue = int.MinValue;
+            int bestRow = -1, bestCol = -1;
+
+            
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (string.IsNullOrEmpty(board[row, col]))
+                    {
+                        board[row, col] = AIPlayer.ToString();
+                        int moveValue = Minimax(0, false, int.MinValue, int.MaxValue);
+                        board[row, col] = null;
+
+                        if (moveValue > bestValue)
+                        {
+                            bestValue = moveValue;
+                            bestRow = row;
+                            bestCol = col;
+                        }
+                    }
+                }
+            }
+
+            
+            buttonGrid[bestRow, bestCol].Text = AIPlayer.ToString();
+            buttonGrid[bestRow, bestCol].Enabled = false;
+            board[bestRow, bestCol] = AIPlayer.ToString();
+
+            
+            CheckGameStatus();
+        }
+
+
+
+
+
+    }
+
+
+
+}
